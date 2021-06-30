963c34be0b3c898c468d7129cfe3e6a6c9aa4e90
Rayyan <85759101+XfaonAE@users.noreply.github.com>
2021-06-30 19:11:23 -0400

    Changed dev checker method

--- src/electron/Main.d.ts
+++ src/electron/Main.d.ts
@@ -1,4 +1,3 @@
 declare const BrowserWindow: typeof Electron.BrowserWindow, app: Electron.App, ipcMain: Electron.IpcMain;
-declare const electronIsDev: any;
 declare const path: any;
 //# sourceMappingURL=Main.d.ts.map
