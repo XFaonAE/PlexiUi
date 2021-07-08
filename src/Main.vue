e9ce1aeb0fcc095864a6ff68b1bd608d34dcd93e
XFaon <85759101+XfaonAE@users.noreply.github.com>
2021-07-08 12:13:28 -0400

    Updated the Default UI once again lol

--- src/Main.vue
+++ src/Main.vue
@@ -8,7 +8,7 @@
 
         <side-bar></side-bar>
 
-        <Frame :title="$config.title" />
+        <Frame ref="_Frame" :title="$config.title" />
         <Gutter>
             <GutterButton @click="plexiuiGutterClick">PlexiUI {{ $package.version }}</GutterButton>
         </Gutter>
@@ -37,7 +37,20 @@ export default {
         plexiuiGutterClick() {
             alert("PlexiUI");
         }
-    }
+    },
+    watch: {
+        $route (to, from) {
+            let path = to.path.split("/")[1];
+            if (path == "") {
+                path = "home";
+            }
+
+            const title = this.$config.title + " - " + path;
+
+            this.$refs._Frame.setTitle(title);
+            document.title = title;
+        }
+    } 
 }
 </script>
 
@@ -58,6 +71,26 @@ body {
 ._view {
     margin-top: 30px;
     height: calc(100vh - 60px);
+    max-height: calc(100vh - 60px);
+    overflow: auto;
     width: 100%;
+
+    &::-webkit-scrollbar {
+        width: 3px;
+        height: 3px;
+    }
+
+    &::-webkit-scrollbar-track {
+        background: @layer0;
+    }
+
+    &::-webkit-scrollbar-thumb {
+        background: @accent;
+    }
+}
+
+::selection {
+    background: @accent;
+    color: @layer0;
 }
 </style>
