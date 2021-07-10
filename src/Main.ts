6bf27d6c8e6676dbd0371b7982da20e757485aeb
XFaon <85759101+XfaonAE@users.noreply.github.com>
2021-07-10 15:41:59 -0400

    Started re-code as a node module

--- src/Main.ts
+++
@@ -1,7 +0,0 @@
-import PlexiUi from "../axeri/plexiui/PlexiUi";
-
-const plexiUi = new PlexiUi();
-plexiUi.setOptions({
-});
-
-plexiUi.run();
--- src/components/ux/Button.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <button class="Button">
-        <slot />
-    </button>
-</template>
-
-<style lang="less" src="../../styles/ux/Button.less"></style>
